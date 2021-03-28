2f3a9eafb465af6ce6882fdfc4a3ba64cb4223d0
lobsterbandit <code.lobsterbandit@outlook.com>
2021-03-28 18:10:07 +0000

    Add initial plex webhook handling

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/lobsterbandit/theater
 
 go 1.16
+
+require github.com/hekmon/plexwebhooks v1.0.0 // indirect
--- server.go
+++ server.go
@@ -1,9 +1,13 @@
 package main
 
 import (
+	"errors"
 	"fmt"
 	"log"
 	"net/http"
+	"time"
+
+	"github.com/hekmon/plexwebhooks"
 )
 
 func createRoute(route string, method string, handler func(http.ResponseWriter, *http.Request)) (string, func(http.ResponseWriter, *http.Request)) {
@@ -29,7 +33,49 @@ func pingHandler(w http.ResponseWriter, r *http.Request) {
 }
 
 func plexWebhookHandler(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "Plex webhook receiver")
+	defer r.Body.Close()
+
+	multiPartReader, err := r.MultipartReader()
+	if err != nil {
+		if errors.Is(err, http.ErrNotMultipart) || errors.Is(err, http.ErrMissingBoundary) {
+			w.WriteHeader(http.StatusBadRequest)
+		} else {
+			w.WriteHeader(http.StatusInternalServerError)
+		}
+
+		_, wErr := w.Write([]byte(err.Error()))
+		if wErr != nil {
+			err = fmt.Errorf("request error: %v | write error: %v", err, wErr)
+		}
+
+		fmt.Println("can't create a multipart reader from request:", err)
+
+		return
+	}
+
+	payload, thumb, err := plexwebhooks.Extract(multiPartReader)
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+
+		_, wErr := w.Write([]byte(err.Error()))
+		if wErr != nil {
+			err = fmt.Errorf("request error: %v | write error: %v", err, wErr)
+		}
+		// Log the error
+		fmt.Println("can't create a multipart reader from request:", err)
+
+		return
+	}
+
+	fmt.Println()
+	fmt.Println(time.Now())
+	fmt.Printf("%+v\n", *payload)
+
+	if thumb != nil {
+		fmt.Printf("Name: %s | Size: %d\n", thumb.Filename, len(thumb.Data))
+	}
+
+	fmt.Println()
 }
 
 func main() {
